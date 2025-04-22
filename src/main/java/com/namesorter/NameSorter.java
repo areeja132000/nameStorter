a65c5eccc713e84c3b09cb59391d43c4a90f4460
Areej Ali <areeja132000@gmail.com>
2025-04-22 15:53:21 -0400

    Update NameSorter.java
    Modularized code more

--- src/main/java/com/namesorter/NameSorter.java
+++ src/main/java/com/namesorter/NameSorter.java
@@ -15,6 +15,22 @@ public class NameSorter {
 	 */
 	public static List<String> sortNames (List<String> allNames) {
 		
+		//Create list of people from list of full name strings
+		List<Person> people = convertStringsToPeople(allNames);
+		
+		//Sort list of people (first by last name, then by given names)
+		people.sort(Comparator.comparing(Person::getLastName).thenComparing(Person::getGivenNames));
+		
+		//Use the sorted list of people to create sorted list of full name strings
+		return convertPeopleToStrings(people);
+	}
+	
+	/**
+	 * Creates a list of Person objects given a list of full names
+	 * @param allNames list of full names
+	 * @return         list of Person objects
+	 */
+	private static List<Person> convertStringsToPeople (List<String> allNames) {
 		//Create empty list of people
 		List<Person> people = new ArrayList<>();
 		
@@ -26,7 +42,6 @@ public class NameSorter {
 				String givenNames = FullNameParser.parseForGivenNames(fullName);
 				String lastName = FullNameParser.parseForLastName(fullName);
 				
-				//Check that parser returned non-null names
 				if (givenNames != null && lastName != null) {
 					
 					//Create new person with associated given and last names
@@ -38,10 +53,16 @@ public class NameSorter {
 			}
 		}
 		
-		//Sort list of people (first by last name, then by given names)
-		people.sort(Comparator.comparing(Person::getLastName).thenComparing(Person::getGivenNames));
+		return people;
 		
-		//Use the sorted list of people to create sorted list of full name strings
+	}
+	
+	/**
+	 * Creates a list of full names given a list of Person objects
+	 * @param people list of Person objects
+	 * @return       list of full names
+	 */
+	private static List<String> convertPeopleToStrings (List<Person> people) {
 		List<String> lines = new ArrayList<>();
 		for (Person person : people) {
 			lines.add(person.getGivenNames() + " " + person.getLastName());
@@ -49,5 +70,4 @@ public class NameSorter {
 		
 		return lines;
 	}
-
 }
